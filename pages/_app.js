8a92b848103eb8fb2b34cb1cae7349ab8feb22b4
Johnny <johnny0929560027@gmail.com>
2024-01-24 17:38:21 +0800

    title

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,10 @@ import "../styles/for_index_test.css";
 import "../styles/map.scss";
 import "../styles/font.css";
 import "../styles/animation.css";
-import "@/styles/engineeringMode.css"
+import "@/styles/engineeringMode.css";
 import { Ubuntu } from "next/font/google";
 import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
 
 const ubuntu = Ubuntu({
   subsets: ["latin"],
@@ -15,11 +16,17 @@ const ubuntu = Ubuntu({
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <main className={ubuntu.className}>
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-    </main>
+    <>
+      <Head>
+        <title>X&X派車系統</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={ubuntu.className}>
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </main>
+    </>
   );
 }
 
